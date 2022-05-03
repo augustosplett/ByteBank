3763e2b07fd66103c097ff15b63932b1ac2b2852
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-05-03 20:10:52 -0300

    criado projeto para funcionarios

---
+++ ByteBank.Funcionarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using ByteBank.Funcionarios.Funcionarios;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "Augusto";
+            funcionario.Salario = 1000.00;
+            Console.WriteLine(funcionario.Nome);
+            Console.WriteLine(funcionario.GetBonificacao());
+        }
+    }
+}
+
