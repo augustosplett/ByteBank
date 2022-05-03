258822e116073463a9cd45960ebd77efa5348766
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-05-03 19:51:29 -0300

    Adicionar arquivos de projeto.

---
+++ 01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,59 @@
+﻿namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int NumeroAgencia { get; set; }
+        public int NumeroConta { get; set; }
+        private double _saldo = 100;
+        public static int TotalDeContasCriadas { get; private set; }
+        public ContaCorrente(Cliente cliente, int agencia, int numero)
+        {
+            Titular = cliente;
+            NumeroAgencia = agencia;
+            NumeroConta = numero;
+            ContaCorrente.TotalDeContasCriadas++;
+        }
+        public double Saldo
+        {
+            get { return _saldo; }
+            set 
+            {
+                if (value > 0) return;
+                _saldo = value; 
+            }
+        }
+  
+        public bool Sacar(double valor)
+        {
+            if(valor <= this._saldo)
+            {
+                _saldo -= valor;
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if(this._saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }   
+}
---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new ContaCorrente(new Cliente("Gabriela", "123.456.789-00", "bancária"),
+                863, 123456);
+
+            conta.Saldo = 100;
+          
+            Console.Write(conta.Titular.Nome);
+            Console.Write(conta.Titular.Cpf);
+        }
+    }
+}
